329ca678b8cb5ec9666d377f73454a893c9c6930
SHAI-MSI\Shai Ben-Sadon <shai.bensadon@gmail.com>
2017-06-24 15:33:34 +0300

    added function skeleton for cleanup of followed tournaments when deleting tournaments permanently.

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,7 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('deleteFollowed', function(req, res) {
+	//add implementation if we want to cleanup this thing
+});
