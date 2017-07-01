07e8dc1c5d46e97a7c8d4a5387edbc4652d40ea8
Adam Edry <adamed123@gmail.com>
2017-07-01 16:22:12 +0300

    Trying a cloud code function

--- cloud/main.js
+++ cloud/main.js
@@ -6,3 +6,30 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('deleteFollowed', function(req, res) {
 	//add implementation if we want to cleanup this thing
 });
+
+Parse.Cloud.define('pingReply', function(request, response) {
+    var params = request.params;
+    var customData = params.customData;
+
+    if (!customData) {
+        response.error("Missing customData!")
+    }
+
+    var jsonData = JSON.parse(customData);
+    var sender = jsonData.sender;
+    var tourName = jsonData.tournamentName;
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo("installationId", sender);
+
+    Parse.Push.send({
+        where: query,
+        // Parse.Push requires a dictionary, not a string.
+        data: {"alert": "You created a tournament named: " + tourName}
+    }, { success: function() {
+        console.log("#### PUSH OK");
+    }, error: function(error) {
+        console.log("#### PUSH ERROR" + error.message);
+    }, useMasterKey: true});
+
+    response.success('success');
+});
