cf8ab3f935aa0a09f22ae9b63102557c94bc1356
Adam Edry <adamed123@gmail.com>
2017-07-01 21:23:26 +0300

    Adding a cloud code function

--- cloud/main.js
+++ cloud/main.js
@@ -23,7 +23,7 @@ Parse.Cloud.define('pushScheduleChanged', function(request, response) {
     Parse.Push.send({
         // where: query,
         // Parse.Push requires a dictionary, not a string.
-        channels: [tournamentId],
+        channels: [tournamentId.toString()],
         data: {"refreshSchedule": true,
                "tournamentId": tournamentId}
     }, { success: function() {
