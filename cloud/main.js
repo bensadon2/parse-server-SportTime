4c39bb71e4fe82b40ac07de28de4d1c0a17b4869
Adam Edry <adamed123@gmail.com>
2017-07-02 14:48:19 +0300

    added a cloud function to test push

--- cloud/main.js
+++ cloud/main.js
@@ -34,3 +34,29 @@ Parse.Cloud.define('pushScheduleChanged', function(request, response) {
 
     response.success('success');
 });
+
+Parse.Cloud.define('testPush', function(request, response) {
+    var params = request.params;
+    var customData = params.customData;
+
+    if (!customData) {
+        response.error("Missing customData!")
+    }
+
+    var jsonData = JSON.parse(customData);
+    var sender = jsonData.sender;
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo("installationId", sender);
+
+    Parse.Push.send({
+        where: query,
+        // Parse.Push requires a dictionary, not a string.
+        data: {"alert": "This is a push notification test"}
+    }, { success: function() {
+        console.log("#### PUSH OK");
+    }, error: function(error) {
+        console.log("#### PUSH ERROR" + error.message);
+    }, useMasterKey: true});
+
+    response.success('success');
+});
